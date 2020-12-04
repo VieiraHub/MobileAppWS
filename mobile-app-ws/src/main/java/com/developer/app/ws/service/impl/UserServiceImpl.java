061bf297026dfe539aff51f0e3204a877e43b175
VieiraHub <vieiraonwork@gmail.com>
2020-12-04 02:03:19 +0000

    Create UserRest, UserDto, UserService, UserServiceImpl and UserEntity.

---
+++ mobile-app-ws/src/main/java/com/developer/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.developer.app.ws.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.developer.app.ws.service.UserService;
+import com.developer.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
