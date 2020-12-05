eb37fca3de32da3f664ca35a5db53daaf2389af0
VieiraHub <vieiraonwork@gmail.com>
2020-12-05 22:39:17 +0000

    Implement GetUser Details Web Service Endpoint and tested.

--- mobile-app-ws/src/main/java/com/developer/app/ws/service/UserService.java
+++ mobile-app-ws/src/main/java/com/developer/app/ws/service/UserService.java
@@ -7,4 +7,5 @@ import com.developer.app.ws.shared.dto.UserDto;
 public interface UserService extends UserDetailsService {
 	UserDto createUser(UserDto user);
 	UserDto getUser(String email);
+	UserDto getUserById(String id);
 }
