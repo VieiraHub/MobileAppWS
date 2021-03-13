8cfc0b873c1771603aed84b002052d377b0885bd
VieiraHub <vieiraonwork@gmail.com>
2021-03-13 00:00:26 +0000

    Add Swagger Dependencies and create Configuration file.

---
+++ mobile-app-ws/src/main/java/com/developer/app/ws/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.developer.app.ws;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	public Docket apiDocket() {
+		Docket socket = new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.developer.app.ws"))
+				.paths(PathSelectors.any())
+				.build();
+		return socket;
+	}
+}
--- mobile-app-ws/src/main/java/com/developer/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/developer/app/ws/ui/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
