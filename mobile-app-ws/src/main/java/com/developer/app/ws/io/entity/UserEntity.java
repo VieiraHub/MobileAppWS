8de78565a97f124e2cfc7138a8bea4d4b43f31f8
VieiraHub <vieiraonwork@gmail.com>
2021-03-13 22:46:16 +0000

    Create Roles: ROLE_ADMIN, ROLE_USER. Create User with Admin Role.

--- mobile-app-ws/src/main/java/com/developer/app/ws/io/entity/UserEntity.java
+++ mobile-app-ws/src/main/java/com/developer/app/ws/io/entity/UserEntity.java
@@ -21,41 +21,37 @@ import javax.persistence.Table;
 public class UserEntity implements Serializable {
 
 	private static final long serialVersionUID = 8126787428414352778L;
-	
+
 	@Id
 	@GeneratedValue
 	private long id;
 
 	@Column(nullable = false)
 	private String userId;
-	
+
 	@Column(nullable = false, length = 50)
 	private String firstName;
-	
+
 	@Column(nullable = false, length = 50)
 	private String lastName;
-	
+
 	@Column(nullable = false, length = 120)
 	private String email;
-	
+
 	@Column(nullable = false)
 	private String encryptedPassword;
-	
+
 	private String emailVerificationToken;
-	
+
 	@Column(nullable = false)
 	private Boolean emailVerificationStatus = false;
-	
+
 	@OneToMany(mappedBy = "userDetails", cascade = CascadeType.ALL)
 	private List<AddressEntity> addresses;
-	
-	@ManyToMany(cascade = {CascadeType.PERSIST}, fetch = FetchType.EAGER)
-	@JoinTable(name = "users_roles", 
-			joinColumns = @JoinColumn(name= "users_id", referencedColumnName = "id"), 
-			inverseJoinColumns = @JoinColumn(name= "roles_id", referencedColumnName = "id"))
-	private Collection<RoleEntity> roles;
 
-	
+	@ManyToMany(cascade = { CascadeType.PERSIST }, fetch = FetchType.EAGER)
+	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "users_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "roles_id", referencedColumnName = "id"))
+	private Collection<RoleEntity> roles;
 
 	public long getId() {
 		return id;
@@ -120,7 +116,7 @@ public class UserEntity implements Serializable {
 	public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
 		this.emailVerificationStatus = emailVerificationStatus;
 	}
-	
+
 	public List<AddressEntity> getAddresses() {
 		return addresses;
 	}
@@ -128,4 +124,13 @@ public class UserEntity implements Serializable {
 	public void setAddresses(List<AddressEntity> addresses) {
 		this.addresses = addresses;
 	}
+
+	public Collection<RoleEntity> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<RoleEntity> roles) {
+		this.roles = roles;
+	}
+
 }
